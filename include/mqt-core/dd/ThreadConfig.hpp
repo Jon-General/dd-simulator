030ae8d7a1174bbbddad78630a5ff5210db25c9c
Jon-General <GeneralJR@bsp.gov.ph>
2025-12-22 03:17:15 +0000

    test

--- include/mqt-core/dd/ThreadConfig.hpp
+++ include/mqt-core/dd/ThreadConfig.hpp
@@ -4,10 +4,16 @@
  * All rights reserved.
  *
  * SPDX-License-Identifier: MIT
+<<<<<<< HEAD
+=======
+ *
+ * Licensed under the MIT License
+>>>>>>> 413c2c13 (test)
  */
 
 #pragma once
 
+<<<<<<< HEAD
 #ifdef _OPENMP
 #include <omp.h>
 #endif
@@ -22,4 +28,27 @@ inline void configureOpenMPThreads(const int desiredThreads) {
   (void)desiredThreads;
 #endif
 }
+=======
+namespace dd {
+
+/**
+ * @brief Resolve the OpenMP thread count requested by the user.
+ *
+ * @param requested Number of threads requested via configuration. A value of
+ *        zero or a negative number indicates that all available processing
+ *        units should be used.
+ * @return The clamped number of threads that should be used at runtime.
+ */
+int resolveOpenMPThreadCount(int requested) noexcept;
+
+/**
+ * @brief Configure the OpenMP runtime for multi-core execution.
+ *
+ * @param requested Number of threads requested via configuration. A value of
+ *        zero or a negative number indicates that all available processing
+ *        units should be used.
+ */
+void configureOpenMPThreads(int requested) noexcept;
+
+>>>>>>> 413c2c13 (test)
 } // namespace dd
---
+++ src/dd/ThreadConfig.cpp
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2023 - 2025 Chair for Design Automation, TUM
+ * Copyright (c) 2025 Munich Quantum Software Company GmbH
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier: MIT
+ *
+ * Licensed under the MIT License
+ */
+
+#include "dd/ThreadConfig.hpp"
+
+#include <algorithm>
+
+#ifdef _OPENMP
+#include <omp.h>
+#endif
+
+namespace dd {
+
+int resolveOpenMPThreadCount(const int requested) noexcept {
+#ifdef _OPENMP
+  if (requested <= 0) {
+    return std::max(1, omp_get_num_procs());
+  }
+  return std::max(1, requested);
+#else
+  (void)requested;
+  return 1;
+#endif
+}
+
+void configureOpenMPThreads(const int requested) noexcept {
+#ifdef _OPENMP
+  omp_set_dynamic(0);
+  omp_set_num_threads(resolveOpenMPThreadCount(requested));
+#else
+  (void)requested;
+#endif
+}
+
+} // namespace dd
